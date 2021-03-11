861e4d0b92c01a20a4bea4df45123b324c0b4351
God-Zalo <martinez.zalocin@gmail.com>
2021-03-11 11:33:26 -0500

    - Added models, serializer, and view - Rollback to Django 2.2

--- ciberc/inventory/models.py
+++ ciberc/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+	serial_number = models.PositiveIntegerField()
+	quantity = models.PositiveIntegerField()
+	price = models.BigIntegerField()
+
+	def __str__(self):
+		return str(self.serial_number)
+
+
+class FileUpload(models.Model):
+	filename = models.PositiveIntegerField()
+	date = models.DateField(auto_now_add=True)
+	file = models.FileField(upload_to='tmp/')
+
+	def __str__(self):
+		return self.filename
--- ciberc/inventory/views.py
+++ ciberc/inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer
+from .models import Product
+from rest_framework import generics
 
 # Create your views here.
+
+
+class InventoryList(generics.ListAPIView):
+	serializer_class = ProductSerializer
+	queryset = Product.objects.all()
